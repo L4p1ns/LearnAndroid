b0df4ef132b42b80b83a11493c19b0de548cdc96
bayendembaseye <bayendemba.seye@koonda.fr>
2019-02-16 11:07:19 +0000

    Ajouter un listener

--- app/src/main/java/com/iam/myapplication/FirstActivity.java
+++ app/src/main/java/com/iam/myapplication/FirstActivity.java
@@ -2,11 +2,20 @@ package com.iam.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+      /*  TextView text  = new TextView(this);
+        text.setText("My Application");
+        String appName = getResources().getResourceName();
+
+        appName = "IAM Android Application";
+        text.setText(appName);
+        setContentView(text);*/
     }
 }
 
--- app/src/main/java/com/iam/myapplication/MainActivity.java
+++ app/src/main/java/com/iam/myapplication/MainActivity.java
@@ -1,13 +1,24 @@
 package com.iam.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button button = findViewById(R.id.btn1);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        };
+        button.setOnClickListener(listener);
+
         setContentView(R.layout.activity_main);
     }
 }
